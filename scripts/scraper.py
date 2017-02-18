db546eaefb45a156f3df51145678ea29e23f2090
Anthony Liang <aliang8@stuy.edu>
2017-02-18 02:02:53 -0500

    7 seconds dick

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,27 +1,47 @@
 import urllib.request
+import re
 
-url = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=wranglers'
+link = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=wranglers'
 
-opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-response = opener.open( url )
-HTML = response.read().decode('utf-8')
+def main():
 
-ul_start = HTML.find('<ul id="s-results-list-atf"')
-ul_end = HTML.find('<ul>', ul_start)
-ul = HTML[ul_start : ul_end]
+	opener = urllib.request.build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+	response = opener.open( link )
+	HTML = response.read().decode('utf-8')
 
-items = ul.split('<a class="a-link-normal a-text-normal"')
+	ul_start = HTML.find('<ul id="s-results-list-atf"')
+	ul_end = HTML.find('<ul>', ul_start)
+	ul = HTML[ul_start : ul_end]
 
-i = 10;
+	items = ul.split('<a class="a-link-normal a-text-normal"')[1:]
 
-for item in items:
-	if i == 0:
-		break
-	if not "jpg" in item:
-		href_start = item.find('https://')
-		href_end = item.find('">', href_start)
-		href = item[href_start : href_end]
-		i -= 1
-		print(href)
+	i = 5;
+	for item in items:
+		if i == 0:
+			break
+		if not "jpg" in item:
+			href_start = item.find('https://')
+			href_end = item.find('">', href_start)
+			href = item[href_start : href_end]
+			i -= 1
+			print(parse(href))
+
+def parse(url):
+    
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    response = opener.open( url )
+    HTML = response.read().decode('utf-8')
+
+    ul_start = HTML.find('<ul class="a-vertical a-spacing-none"')
+    ul_end = HTML.find('</ul>',ul_start)
+    ul = HTML[ul_start:ul_end]
+    ul = ul.split('<li><span class="a-list-item">')[1:]
+
+    for item in ul: 
+        if '%' in item:
+            return item.replace('</span></li>','').strip()
+
+main()
 
