9311c44b2cc89ff34ff53a770cad3b62f67b326b
Nicholas Yang <nick@nicholasyang.com>
2017-02-18 06:41:13 -0500

    more css. added products page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218035520) do
+ActiveRecord::Schema.define(version: 20170218111427) do
 
   create_table "products", force: :cascade do |t|
     t.string   "picture"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170218035520) do
     t.string   "material"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+products = Product.create([
+                            {picture: 'http://lp.hm.com/hmprod?set=key[source],value[/model/2016/E00%200317980%20005%2030%201243.jpg]&set=key[rotate],value[]&set=key[width],value[]&set=key[height],value[]&set=key[x],value[]&set=key[y],value[]&set=key[type],value[STILL_LIFE_FRONT]&set=key[hmver],value[6]&call=url[file:/product/large',
+                             price: 18,
+                             score: 20,
+                             link: "https://www.horriblyunderqualified.com",
+                             material: "Gold",
+                             name: "Shoe"}
+                          ])
