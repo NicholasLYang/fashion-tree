a7301e26eab50a0fb2f8cc638653593e9d68398e
Anthony Liang <aliang8@stuy.edu>
2017-02-18 01:41:59 -0500

    3

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-url = 'https://www.amazon.com/Wrangler-Authentics-Classic-Regular-Stonewash/dp/B00XKXN95W/ref=sr_1_2?ie=UTF8&qid=1487393556&sr=8-2&keywords=wranglers'
-
-opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-response = opener.open( url )
-html_contents = response.read()
-
