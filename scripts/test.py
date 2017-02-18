a0181f3c49fcf12facf9a93d2ac5020a442568b8
jkao1 <jkao1@stuy.edu>
2017-02-18 06:31:01 -0500

    finished parse_item

--- scripts/test.py
+++ scripts/test.py
@@ -1,31 +1,52 @@
-<<<<<<< HEAD
 import urllib2
 import re
-=======
-import urllib.request
->>>>>>> eb58ef21ae92a2111fad0b893020d3130ed60949
 
-url='https://www.amazon.com/Nautica-Jeans-Relaxed-Rigger-34Wx30L/dp/B005BNJ5XY/ref=sr_1_13?s=apparel&ie=UTF8&qid=1487398326&sr=1-13&nodeID=7141123011&keywords=jeans&refinements=p_n_feature_eighteen_browse-bin%3A14630392011'
-def main():
-    
+url='https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=pants&rh=i%3Aaps%2Ck%3Apants'
+def parse(url):
     opener = urllib2.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     response = opener.open( url )
     HTML = response.read()
 
-<<<<<<< HEAD
+    ul_start = HTML.find('<ul id="s-results-list-atf"')
+    ul_end = HTML.find('</ul>',ul_start)
+    ul = HTML[ul_start:ul_end]
+    ul = ul.split('<li id="result_')[1:]
+
+    D = []
+    for item in ul:
+        link_start = item.find('<a class="a-size-small a-link-normal a-text-normal" href="') + 58 
+        link_end = item.find('">',link_start)
+        link = item[link_start:link_end]
+        D.append(parse_item(link))
+    return D
+
+def parse_item(url):
+    print("parsing "+url)
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    response = opener.open( url )
+    HTML = response.read()
+    M = {}
+
     ul_start = HTML.find('<ul class="a-vertical a-spacing-none"')
     ul_end = HTML.find('</ul>',ul_start)
     ul = HTML[ul_start:ul_end]
     ul = ul.split('<li><span class="a-list-item">')[1:]
-=======
-opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-response = opener.open( url )
-html_contents = response.read()
->>>>>>> eb58ef21ae92a2111fad0b893020d3130ed60949
-
     for item in ul: 
         if '%' in item:
-            return item.replace('</span></li>','').strip()
-main()
+            M["MATERIAL"] = item.replace('</span></li>','').strip()
+
+    title_start = HTML.find('<span id="productTitle" class="a-size')
+    title_end = HTML.find('</span>',title_start)
+    title = HTML[title_start:title_end][45:].strip()
+    M["NAME"] = title
+
+    price_start = HTML.find('<span id="priceblock_ourprice"')
+    price_end =HTML.find('</span>',price_start)
+    price = HTML[price_start:price_end]
+    price = price[price.find('>')+1:]
+    M["PRICE"] = price
+
+    return M
+
