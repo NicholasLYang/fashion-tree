9fa71f716df6a2ea2cb08d6ab210de817131214b
jkao1 <jkao1@stuy.edu>
2017-02-18 17:03:32 -0500

    finished hm

--- scripts/hm.py
+++ scripts/hm.py
@@ -3,18 +3,27 @@ import urllib.request
 import requests
 import re
 
-url = "http://www.hm.com/us/product/20775?article=20775-D"
+def main():
+    output = "picture,price,score,link,material,name,keyword"
+    queries = ['Sweaters & Cardigans', 'Shirts', 'Jeans', 'Jackets & Coats', 'Hoodies & Sweatshirts', 'Pants', 'T-shirts & Tank tops', 'Basics', 'Jackets & Suits', 'Accessories', 'Shoes', 'Underwear & Loungewear', 'Sportswear', 'Swimwear', 'Shorts', 'Casual', 'Divided', 'H&M Man', 'Modern Classics']
+    output = "picture,price,score,link,material,name,keyword"
+    for q in queries:
+        output += query(q)
+    f = open('hm.csv','w',errors='ignore')
+    f.write(output)
+    f.close()
 
-def query(keyword):
-    url = "http://www.hm.com/us/products/search?q=" + keyword
+
+def query(q):    
+    url = 'http://www.hm.com/us/products/search?q=' + q
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
-    output = "picture,price,score,link,material,name,keyword"
-
+    
     links = soup.find_all('a', {'target': '_self'})
+    output = ""
     for link in links:
-        output += "\n" + parse_item(link.get('href')) + keyword
-
+        output += "\n" + parse_item(link.get('href'))
+    print("-----COMPLETED " + q)
     return output
     
 def parse_item(url):
@@ -33,7 +42,7 @@ def parse_item(url):
     row += price + ","
 
     material_start = HTML.find('text-information') + 18
-    material = HTML[material_start:HTML.find('.',material_start)]
+    material = HTML[material_start:HTML.find('.',material_start)].replace(',',' ')
 
     row += str(computeScore(material)) + ","
     row += url + ","
@@ -42,7 +51,8 @@ def parse_item(url):
     name_end = HTML.find('<span class="price"')
     name = HTML[HTML.rfind('>',name_end-100,name_end)+1:name_end].strip()
     row += name + ","
-
+    row += "," #keywords
+    print("processed " + name)
     return row
 
 def computeScore(material):
@@ -52,7 +62,11 @@ def computeScore(material):
         'polyester': 109,
         'viscose': 71,
         'spandex': 135,
-        'rayon': 68
+        'rayon': 68,
+        'acrylic': 2,
+        'nylon': 68,
+        'metallic fiber': 536,
+        'lyocell': 36,
         }
     water_consumption = {
         'cotton': 12500,
@@ -60,15 +74,20 @@ def computeScore(material):
         'polyester': 0,
         'viscose': 640,
         'spandex': 26,
-        'rayon': 190
+        'rayon': 190,
+        'acrylic': 2,
+        'nylon': 2,
+        'metallic fiber': 12,
+        'lyocell':12,        
         }    
-    materials = material.split(', ')
+    materials = material.split('  ')
     score = 0.0
     for m in materials:
         m = m.split('% ')
-        score += int(m[0]) / 100 * (energy_consumption[m[1]] + water_consumption[m[1]]) / 2
+        try:
+            score += int(m[0]) / 100 * (energy_consumption[m[1]] + water_consumption[m[1]]) / 2
+        except:
+            score += 6.12
     return score
 
-f = open('ex.csv','w',errors='ignore')
-f.write(query('jeans'))
-f.close()
+main()
